4261f0846dec92df14829455cdefe2d35e2079a9
Andrea Arcila Cano <andreaac777@gmail.com>
2026-01-25 22:11:31 -0500

    Customers page and Sentry added

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -8,6 +8,8 @@ import { BrowserRouter } from 'react-router'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+import * as Sentry from "@sentry/react";
+
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
@@ -15,6 +17,19 @@ if (!PUBLISHABLE_KEY) {
 }
 
 const queryClient = new QueryClient()
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+  enableLogs: true,
+
+  integrations: [Sentry.replayIntegration()],
+  // Session Replay
+  replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0,
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
