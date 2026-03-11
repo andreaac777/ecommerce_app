d6b4e46c50628bc37a25beef944f8ac32d3ec179
Andrea Arcila Cano <andreaac777@gmail.com>
2026-03-11 10:41:23 -0500

    Fixes in server.js and app.json added

--- backend/src/server.js
+++ backend/src/server.js
@@ -112,13 +112,12 @@ app.get("/api/health", (req, res) => {
 
 // Make app ready for deployment
 if (ENV.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../admin/dist")));
-    app.get("/{*any}", (req, res) => {
-        res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
-    });
+  app.use(express.static(path.join(__dirname, "../admin/dist")));
+  app.get("/{*any}", (req, res) => {
+      res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+  });
 }
 
-// Detecta automáticamente todas las IPs de red disponibles
 const getNetworkIPs = () => {
   const interfaces = os.networkInterfaces();
   const ips = [];
